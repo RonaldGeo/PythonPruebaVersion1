a07c09bbb5ed04dc616f740e484b8d4236296f08
Ronald Ordoñez <rgox369@hotmail.com>
2018-09-08 13:39:39 -0600

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__) #nuevo objeto
+app = Flask(__name__) 
 
-@app.route('http://35.196.160.196:8080/') #wrap o un decorador
-
+@app.route('/') 
 def index():
-	return 'HOla mundo' #regresa un string
+	return 'HOLAAAAAAAAAAAA' 
 
-app.run() #ejecuta el servidor en puerto 5000
-
+if __name__ == '__main__':
+	app.run(debug = True, port = 8080)
 
